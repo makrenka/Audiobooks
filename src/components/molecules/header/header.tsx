15106ac39f3ea8a43db8bc86fd16668f986430d6
makrenka <smokrenko79@gmail.com>
2023-02-22 19:07:16 +0300

    add fonts and header

---
+++ src/components/molecules/header/header.tsx
@@ -0,0 +1,29 @@
+import { HandySvg } from "handy-svg";
+import logo from '../../../assets/images/icons/logo.svg';
+import settings from '../../../assets/images/icons/settings.svg';
+
+export const Header = () => (
+    <header className="header">
+        <div className="container">
+            <div className="header__wrapper">
+                <div className="header___logo">
+                    <HandySvg
+                        src={logo}
+                        className="header___logo-icon"
+                        width="40"
+                        height="40"
+                    />
+                    <h1 className="header__heading">
+                        AudiBooks
+                    </h1>
+                </div>
+                <HandySvg
+                    src={settings}
+                    className="header__settings-icon"
+                    width="24"
+                    height="24"
+                />
+            </div>
+        </div>
+    </header>
+)
