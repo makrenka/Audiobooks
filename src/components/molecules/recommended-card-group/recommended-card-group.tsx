d1ec05412d6f96caf0044cc8a36f38915c6c3d40
makrenka <smokrenko79@gmail.com>
2023-03-19 12:35:13 +0300

    add state to the detail-page

--- src/components/molecules/recommended-card-group/recommended-card-group.tsx
+++ src/components/molecules/recommended-card-group/recommended-card-group.tsx
@@ -5,8 +5,8 @@ import './recommended-card-group.css';
 export const RecommendedCardGroup = () => (
     <div className="recommended-home__images">
         {audiobooks.map(({ coverBigSize, id }) =>
-            <Link to={"detail/" + id}>
-                <img src={coverBigSize} alt="Cover of the book" className='recommended-home__img' key={id} />
+            <Link to={"detail/" + id} key={id}>
+                <img src={coverBigSize} alt="Cover of the book" className='recommended-home__img' />
             </Link>
         )}
     </div>
--- src/components/pages/detail-page/detail-page.tsx
+++ src/components/pages/detail-page/detail-page.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { audiobooks } from "../../../constants/audiobooks";
 import { HeaderDetail } from "../../molecules/header-detail";
 
-export const DetailPage = () => (
-    <>
-        <HeaderDetail />
-    </>
-)
+export const DetailPage = () => {
+    const params = useParams();
+    const bookId = params.id;
+
+    const [book, setBook] = useState({});
+
+    useEffect(() => {
+        const book = audiobooks.filter((item) => item.id == bookId);
+        setBook(book);
+    }, [])
+
+    return (
+        <>
+            <HeaderDetail />
+        </>
+    )
+}
